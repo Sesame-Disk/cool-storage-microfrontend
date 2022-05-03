3600eb7578c133bb03c837e5d949043d822df8bf
alvaldes <angelluis2605@gmail.com>
2022-05-03 02:32:20 -0400

    Fixed linebreaks in css modules

--- src/pages/NotMatch.jsx
+++ src/pages/NotMatch.jsx
@@ -1,14 +1,10 @@
-import { useLocation } from "react-router-dom";
 import styles from "./NotMatch.module.css";
 
 const NotMatch = () => {
-  const location = useLocation();
   return (
     <div>
       <h1 className={styles["title"]}>404</h1>
-      <h2 className={styles["subtitle"]}>
-        Page ({location.pathname}) not found
-      </h2>
+      <h2 className={styles["subtitle"]}>Page not found</h2>
     </div>
   );
 };
